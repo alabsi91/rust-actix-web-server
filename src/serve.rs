1798eb3218114890680c00b5149e771152824fb4
alabsi91 <alabsi91@gmail.com>
2024-09-08 16:54:23 +0300

    Server files at the root of the "public" folder

--- src/serve.rs
+++ src/serve.rs
@@ -5,10 +5,18 @@ use std::path::PathBuf;
 use crate::config::CONFIG;
 
 pub async fn file_handler((req, path): (HttpRequest, web::Path<String>)) -> Result<NamedFile> {
-    let file_path: PathBuf = PathBuf::from(&CONFIG.public_dir).join(path.as_str());
-
     let connection_info = req.connection_info();
     let client_ip = connection_info.realip_remote_addr().unwrap_or("unknown ip");
+
+    // first check without the trailing slash, by removing it
+    let file_path_no_slash =
+        PathBuf::from(&CONFIG.public_dir).join(path.as_str().trim_end_matches('/'));
+    if file_path_no_slash.exists() && file_path_no_slash.is_file() {
+        println!("[{}] Serving: {}", client_ip, file_path_no_slash.display());
+        return Ok(NamedFile::open(file_path_no_slash)?);
+    }
+
+    let file_path: PathBuf = PathBuf::from(&CONFIG.public_dir).join(path.as_str());
 
     // serve assets
     if file_path.exists() && file_path.is_file() {
